3258e85fa880f0893fd1836f962355a07c805904
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2024-01-01 22:32:31 +0330

    create contact form

--- components/contact-us.vue
+++ components/contact-us.vue
@@ -1,113 +1,109 @@
 <template>
-  <div class="client d-flex justify-content-center mt-5 ">
-    <div id="maps" class="map col-9 row align-items-center  shadow-lg rounded">
-
-    </div>
+  <div class="client d-flex justify-content-center mt-5">
+    <div
+      id="maps"
+      class="map col-9 row align-items-center shadow-lg rounded"
+    ></div>
   </div>
   <div class="client d-flex justify-content-center mt-5 mb-5">
-    <div class="contact col-9 row align-items-center  shadow-lg rounded">
-
+    <div class="contact col-9 row align-items-center shadow-lg rounded">
+      <div class="col-7">
+        <h2>تماس با ما</h2>
+        <p>نام و نام خانوادگی</p>
+        <input type="text" placeholder="نام و نام خانوادگی خود را وارد نمایید" />
+        <p>آدرس ایمیل</p>
+        <input type="text" placeholder="آدرس ایمیل خود را وارد نمایید" />
+        <p>تلفن همراه</p>
+        <input type="text" placeholder="شماره خود را وارد کنید" />
+        <p>پیام شما</p>
+        <textarea type="text" placeholder="پیام خود را برای ما بنویسید" />
+        <p id="btn">ارسال پیام</p>
+      </div>
+      <div class="col-5">
+        <p>    آدرس ایمیل : hamidreza.eidy1999@gmail.com</p>
+        <p>پشتیبانی : 02155225522</p>
+        <p>پیشنهادات و انتقادات : داخلی 100</p>
+        <p>آدرس دفتر مرکزی : تهران، ولیعصر</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.map{
+#btn{
+  background-color: #00a693 ;
+  width: 120px;
+  font-size: 20px;
+text-align: center;
+padding: 5px 15px;
+border-radius: 10px;
+}
+.map {
   height: 30vh;
 }
-.contact{
-  height: 60vh;
+.contact {
+padding: 50px 10px;
+
 }
 </style>
 
 <script setup>
 const { $leaflet } = useNuxtApp();
-
 onMounted(() => {
-    let map = $leaflet.map('maps').setView([35.700105, 51.400394], 14)
-    
-    // let tiles = $leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    //     maxZoom: 18,
-    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-    //         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //     id: 'mapbox/streets-v11',
-    //     tileSize: 512,
-    //     zoomOffset: -1
-    // }).addTo(map);
+  let map = $leaflet.map("maps").setView([35.700105, 51.400394], 14);
 
-    let tiles = $leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-    }).addTo(map);
+  // let tiles = $leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  //     maxZoom: 18,
+  //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+  //         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //     id: 'mapbox/streets-v11',
+  //     tileSize: 512,
+  //     zoomOffset: -1
+  // }).addTo(map);
 
-    let marker = $leaflet.marker([35.700105, 51.400394], {
-        icon: $leaflet.icon({
-            popupAnchor: [12, 6],
-            iconUrl: '/map/marker-icon.png',
-            shadowUrl: '/map/marker-shadow.png'
-        })
-    }).addTo(map)
-   })
-   
+  let tiles = $leaflet
+    .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 18,
+    })
+    .addTo(map);
+
+  let marker = $leaflet
+    .marker([35.700105, 51.400394], {
+      icon: $leaflet.icon({
+        popupAnchor: [12, 6],
+        iconUrl: "/map/marker-icon.png",
+        shadowUrl: "/map/marker-shadow.png",
+      }),
+    })
+    .addTo(map);
+});
 </script>
 
 <style lang="scss" scoped>
+p{
+  margin-top: 30px;
+}
+input {
+  width: 450px;
+  height: 37px;
+  border-radius: 18px;
+  border: none;
+  padding-right: 17px;
+  font-size: 13px;
 
-.leaflet-bar{
-  box-shadow: none;
+  font-family: "Yekan";
+  background-color: #737373;
 }
-.leaflet-control-zoom a{
-  box-shadow: 0 1px 5px rgba(0,0,0,0.65); 
+textarea{
+  border-radius: 18px;
+  border: none;
+  padding-right: 10px;
+  font-size: 13px;
+  width: 450px;
+  font-family: "Yekan";
+  background-color: #737373;
 }
-.leaflet-bar a{
-  border-radius: 4px;
-  background-color: #002244;
-  border: 1px solid rgba(0,0,0,.15);
-  color: #FB4F14;
+input:focus{
+  outline: none;
 }
-.leaflet-bar a:last-child{
-  border: 1px solid rgba(0,0,0,.15);
-}
-.leaflet-bar a:hover{
-  background-color: #FB4F14;
-  border: 1px solid rgba(0,0,0,.15);
-  color: #002244;
-}
-.leaflet-control-zoom-in{
-  margin-top: 10px;
-}
-.leaflet-control-zoom-out{
-  margin-top: 15px;
-}
-.leaflet-control-zoom-in{
-  color: black;
-  background-color: aqua;
-  size: 10px;
-}
-span{
-  font-size: 10px !important; 
-}
-#map{
-
-  a{
-    font-size: 10px;
-  }
-}
-.leaflet-control-zoom-in{
-  font-size: 10px !important;
-}
-.leaflet-control-zoom-in,
-.leaflet-control-zoom-out {
-  font: bold 10px 'Lucida Console', Monaco, monospace;
-  text-indent: 1px;
-  }
-.leaflet-control-zoom-out {
-  font-size: 10px;
-  }
-
-.leaflet-touch .leaflet-control-zoom-in {
-  font-size: 12px;
-  }
-.leaflet-touch .leaflet-control-zoom-out {
-  font-size: 14px;
-  }
 </style>
